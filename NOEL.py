4724dc3f4e6b188859ac5812b64062e0201afb01
Azim Khan(Nil) <119257178+Azimkhan404@users.noreply.github.com>
2024-02-28 16:58:21 +0600

    Delete NOEL.py

--- NOEL.py
+++
@@ -1,9 +0,0 @@
-import os,platform
-os.system('git pull')
- 
-NOEL=platform.architecture()[0]
-if NOEL=="32bit":
-    print('Sorry 32 Bit Not Supported...')
-elif NOEL=="64bit":
-     __import__("NOEL")
- 
