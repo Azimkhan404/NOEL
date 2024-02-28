4d38c0d1b5354ca3f54c01dc030f50d0ce570444
Azim Khan(Nil) <119257178+Azimkhan404@users.noreply.github.com>
2024-02-28 16:44:53 +0600

    Add files via upload

---
+++ NOEL.py
@@ -0,0 +1,9 @@
+import os,platform
+os.system('git pull')
+ 
+jkw=platform.architecture()[0]
+if NOEL=="32bit":
+    print('Sorry 32 Bit Not Supported...')
+elif NOEL=="64bit":
+     __import__("NOEL")
+ 
