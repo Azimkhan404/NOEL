801cf783ce6bde849f0b393c23a829ceb76f963d
Azim Khan(Nil) <119257178+Azimkhan404@users.noreply.github.com>
2024-02-28 16:47:00 +0600

    Update NOEL.py

--- NOEL.py
+++ NOEL.py
@@ -1,7 +1,7 @@
 import os,platform
 os.system('git pull')
  
-jkw=platform.architecture()[0]
+NOEL=platform.architecture()[0]
 if NOEL=="32bit":
     print('Sorry 32 Bit Not Supported...')
 elif NOEL=="64bit":
